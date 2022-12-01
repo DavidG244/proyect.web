8069451a765823bc4411d3cb64608d7ea5aee77f
DavidG244 <119634843+DavidG244@users.noreply.github.com>
2022-12-01 18:47:52 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,87 @@
+window.addEventListener("load", function(){
+    document.getElementById("loader").classList.toggle("loader2")
+})
+var cerrar = document.querySelector('.cerrar');
+var boton1 = document.querySelector('.boton1');
+var boton2 = document.querySelector('.boton2');
+var boton3 = document.querySelector('.boton3');
+var boton4 = document.querySelector('.boton4');
+var boton5 = document.querySelector('.boton5');
+var boton6 = document.querySelector('.boton6');
+var boton7 = document.querySelector('.boton7');
+var boton8 = document.querySelector('.boton8');
+var boton9 = document.querySelector('.boton9');
+var boton10 = document.querySelector('.boton10');
+var boton11 = document.querySelector('.boton11');
+var boton12 = document.querySelector('.boton12');
+var modal = document.querySelector('.modal');
+
+cerrar.addEventListener("click", ()=> {
+    modal.classList.add('ocultar');
+    modal.classList.remove('visible');
+    
+})
+
+boton1.addEventListener("click", ()=> {
+    modal.classList.add('visible');
+    modal.classList.remove('ocultar');
+    
+})
+boton2.addEventListener("click", ()=> {
+    modal.classList.add('visible');
+    modal.classList.remove('ocultar');
+    
+})
+boton3.addEventListener("click", ()=> {
+    modal.classList.add('visible');
+    modal.classList.remove('ocultar');
+    
+})
+boton4.addEventListener("click", ()=> {
+    modal.classList.add('visible');
+    modal.classList.remove('ocultar');
+    
+})
+boton5.addEventListener("click", ()=> {
+    modal.classList.add('visible');
+    modal.classList.remove('ocultar');
+    
+})
+boton6.addEventListener("click", ()=> {
+    modal.classList.add('visible');
+    modal.classList.remove('ocultar');
+    
+})
+boton7.addEventListener("click", ()=> {
+    modal.classList.add('visible');
+    modal.classList.remove('ocultar');
+    
+})
+boton8.addEventListener("click", ()=> {
+    modal.classList.add('visible');
+    modal.classList.remove('ocultar');
+    
+})
+boton9.addEventListener("click", ()=> {
+    modal.classList.add('visible');
+    modal.classList.remove('ocultar');
+    
+})
+boton10.addEventListener("click", ()=> {
+    modal.classList.add('visible');
+    modal.classList.remove('ocultar');
+    
+})
+boton11.addEventListener("click", ()=> {
+    modal.classList.add('visible');
+    modal.classList.remove('ocultar');
+    
+})
+boton12.addEventListener("click", ()=> {
+    modal.classList.add('visible');
+    modal.classList.remove('ocultar');
+    
+})
+
+
+
